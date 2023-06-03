8905b76af73776d1f1a7546a4a423fa2e5273e13
Asfandyar-Khan-2022 <117299102+Asfandyar-Khan-2022@users.noreply.github.com>
2023-06-03 15:19:38 +0100

    tracking hand and specific points

--- hand_tracking_min.py
+++ hand_tracking_min.py
@@ -5,9 +5,12 @@ import time
 # define a video capture object
 vid = cv2.VideoCapture(0)
 
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while(True):
 
@@ -24,10 +27,49 @@ while(True):
 
     # if a hand is detected then do the following
     if results.multi_hand_landmarks:
+
         # handLms contains data for each hand displayed on the webcam
         for handLms in results.multi_hand_landmarks:
-            # draw the points and draw a line through them. using mpDraw function
-            mpDraw.draw_landmarks(frame, handLms, mp_hands.HAND_CONNECTIONS)
+
+            # get the landmarks and the id number
+            for id, lm in enumerate(handLms.landmark):
+
+                # ratio of pixel value
+                print(id,lm)
+
+                # multiply the ratio to get the pixel value
+                # hight, width, channel
+                h, w, c = frame.shape
+
+                # postion of center, the position of each point can be found
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+
+                # if the id value is zero (landmark 0) then draw a purple dot of size 20
+                if int(id) == 0:
+                    cv2.circle(frame, (cx, cy), 20, (255, 0, 255), cv2.FILLED)
+                    
+
+
+
+
+
+
+            # draw the points and draw a line through them
+            # connect the lines using HAND_CONNECTIONS
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+    
+    # get the current time
+    cTime = time.time()
+
+    # the time different allows for the fps to be found
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    # put the fps on the window and define how it will look 
+    # and where on the window it will be placed
+    cv2.putText(frame,str(int(fps)),(10, 70), cv2.FONT_HERSHEY_PLAIN,3,
+                (255,0,255),3)
 
     # show the captured frame
     cv2.imshow('Image', frame)
@@ -38,5 +80,6 @@ while(True):
 
 # close the cap(the webcam)
 vid.release()
+
 # destroy all windows
 cv2.destroyAllWindows()
